22bcd81e9379f7c16a7f5d526f60ab54667dba1d
Melissa <melifisher1234@gmail.com>
2024-06-27 16:35:24 -0400

    salas

--- src/main/java/com/mycompany/domibo/ManejadorDeConexionesBD.java
+++ src/main/java/com/mycompany/domibo/ManejadorDeConexionesBD.java
@@ -19,47 +19,45 @@ import org.json.JSONObject;
 public class ManejadorDeConexionesBD {
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/socket_database";
     private static final String USER = "postgres";
-    private static final String PASS = "123";
+    private static final String PASS = "password";
     
-    public boolean registrarJugadorEnLaBD(String parametros) {
-        JSONObject parametrosJson = new JSONObject(parametros);
-        String nombre = parametrosJson.getString("nombre");
-        String nombreDeUsuario = parametrosJson.getString("nombreDeUsuario");
-        String correoElectronico = parametrosJson.getString("correoElectronico");
-        String telefono = parametrosJson.getString("telefono");
-        String contrasena = parametrosJson.getString("contrasena");
+    public int registrarJugadorEnLaBD(String nombre, String nombreDeUsuario, String correoElectronico, String telefono, String contrasena) {
         try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
-            String query = "INSERT INTO public.users (name,username, email, telefono, password) VALUES (?, ?, ?, ?, ?)";
+            String query = "INSERT INTO public.users (name,username, email, telefono, password) VALUES (?, ?, ?, ?, ?) RETURNING id";
             PreparedStatement statement = conn.prepareStatement(query);
             statement.setString(1, nombre);
             statement.setString(2, nombreDeUsuario);
             statement.setString(3, correoElectronico);
             statement.setString(4, telefono);
             statement.setString(5, contrasena);
-            int rowsInserted = statement.executeUpdate();
-            return rowsInserted > 0;
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+            return 0;
         } catch (SQLException e) {
             System.out.println("Error al registrar el usuario en la BD.");
             e.printStackTrace();
-            return false;
+            return 0;
         }
     }
     
-    public boolean verificarInicioDeSesion(String parametros) {
-        JSONObject parametrosJson = new JSONObject(parametros);
-        String nombreDeUsuario = parametrosJson.getString("nombreDeUsuario");
-        String contrasena = parametrosJson.getString("contrasena");
+    public ResultSet verificarInicioDeSesion(String nombreDeUsuario, String contrasena) {
         try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
             String query = "SELECT * FROM public.users WHERE username = ? AND password = ?";
             PreparedStatement statement = conn.prepareStatement(query);
             statement.setString(1, nombreDeUsuario);
             statement.setString(2, contrasena);
             ResultSet resultSet = statement.executeQuery();
-            return resultSet.next();
+            if (resultSet.next()) {
+                return resultSet;
+            }else{
+                return null;
+            }
         } catch (SQLException e) {
             System.out.println("Error al iniciar sesión con el usuario en la BD.");
             e.printStackTrace();
-            return false;
+            return null;
         }
     }
 }
